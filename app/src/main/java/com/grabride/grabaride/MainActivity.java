b08a3356108faadc48b5612919c6ee8bd21afe30
kumarPuneeth <puneeth.kumar333@gmail.com>
2017-08-12 15:20:56 +0530

    pickup and drop date generation

--- app/src/main/java/com/grabride/grabaride/MainActivity.java
+++ app/src/main/java/com/grabride/grabaride/MainActivity.java
@@ -1,13 +1,103 @@
 package com.grabride.grabaride;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Calendar c;
+    EditText from, to;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_adaptor);
+
+        from = (EditText) findViewById(R.id.selectFrom);
+        to = (EditText) findViewById(R.id.selectTo);
+
+        c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        from.setOnClickListener(new View.OnClickListener() {
+            String pickupDate, pickupTime, dropDate, dropTime;
+
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        pickupDate = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+
+                        mHour = c.get(Calendar.HOUR_OF_DAY);
+                        mMinute = c.get(Calendar.MINUTE);
+
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                                pickupTime = hourOfDay + ":" + minute;
+
+                                from.setText(pickupDate + "\n" + pickupTime);
+                            }
+                        }, mHour, mMinute, false);
+                        timePickerDialog.show();
+
+                    }
+                }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+
+        });
+
+
+        to.setOnClickListener(new View.OnClickListener() {
+            String dropDate, dropTime;
+
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        dropDate = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+
+                        mHour = c.get(Calendar.HOUR_OF_DAY);
+                        mMinute = c.get(Calendar.MINUTE);
+
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                                dropTime = hourOfDay + ":" + minute;
+
+                                to.setText(dropDate + "\n" + dropTime);
+                            }
+                        }, mHour, mMinute, false);
+                        timePickerDialog.show();
+
+                    }
+                }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+
+        });
+
     }
+
 }
--- app/src/test/java/com/grabride/grabaride/ExampleUnitTest.java
+++ app/src/test/java/com/grabride/grabaride/ExampleUnitTest.java
@@ -2,6 +2,7 @@ package com.grabride.grabaride;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 /**
